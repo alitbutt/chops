bafd7ae27251acb7ff6127433fc234082f7bc5b4
Ali Butt <alitbutt@users.noreply.github.com>
2022-10-13 21:35:05 -0700

    Create union-find.cpp

---
+++ union-find.cpp
@@ -0,0 +1,45 @@
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz), rank(sz), size(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        while (x != root[x]) {
+            x = root[x];
+        }
+        return x;
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            size--;
+        }
+    }
+    
+    int getSize() {
+        return size;
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+private:
+    vector<int> root;
+    vector<int> rank;
+    int size;
+};
